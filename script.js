a2c23deab18fe7ced243a863d702bd40c37216c2
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-04 20:52:18 -0800

    feat: initial version

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Remove redundant information from repository, homepage, bugs fields in package.json
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
